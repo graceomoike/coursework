3f8ed65eafe5bc7a9155f5bf0a8dd22e7c8f5bfb
graceomoike <graceomoikee@gmail.com>
2024-11-08 14:59:44 +0000

    fixed error

--- TestRunner.java
+++ TestRunner.java
@@ -1,34 +1,35 @@
 import org.junit.*;
-import java.lang.Exception;
 import org.junit.runner.JUnitCore;
 import org.junit.runner.Result;
 import org.junit.runner.notification.Failure;
 import java.util.logging.Logger;
 import java.util.logging.Level;
 
-private static final Logger logger = Logger.getLogger(YourClassName.class.getName());
-
-
 public class TestRunner {
-	public static void main(String[] args) {
-		Result result = JUnitCore.runClasses(Dex2HexTest.class);
-		int fails = 0;
-		try {
-			for (Failure failure : result.getFailures()) {
-				System.out.println(failure.toString());
-				fails++;
 
-		    }
-			if (fails > 0) {
-			    logger.severe(fails + " tests failed");
-			    System.exit(1);
-	            } else {
-    			logger.info("All tests passed successfully.");
-    			System.exit(0);
-		    }
+    private static final Logger logger = Logger.getLogger(TestRunner.class.getName());
 
+    public static void main(String[] args) {
+        Result result = JUnitCore.runClasses(Dex2HexTest.class);
+        int fails = 0;
+        
+        try {
+            for (Failure failure : result.getFailures()) {
+                logger.severe(failure.toString());
+                fails++;
+            }
+            
+            if (fails > 0) {
+                logger.severe(fails + " tests failed");
+                System.exit(1);
+            } else {
+                logger.info("All tests passed successfully.");
+                System.exit(0);
+            }
 
-
-}
-}
+        } catch (Exception e) {
+            logger.log(Level.SEVERE, "An error occurred during test execution: ", e);
+            System.exit(1);
+        }
+    }
 }
