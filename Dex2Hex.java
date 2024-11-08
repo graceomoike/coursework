f750f77c34123a1e99996ed4698b32b5caac3cc2
graceomoike <graceomoikee@gmail.com>
2024-11-08 15:38:26 +0000

    fixed error and adressed sonarqube feedback

--- Dex2Hex.java
+++ Dex2Hex.java
@@ -3,13 +3,12 @@ import java.util.logging.Level;
 
 
  class Dex2Hex {
-    public static void main(String args[]) {
 
-        private static final Logger logger = Logger.getLogger(Dex2Hex.class.getName());
+    private static final Logger logger = Logger.getLogger(Dex2Hex.class.getName());
 
+        public static void main(String[] args) {
 
 	// Check if no input is provided
-        public static void main(String[] args) {
         if (args.length == 0) {
             logger.severe("Error: No input provided. Please enter an integer value.");
             System.exit(1); // Exit or handle as appropriate
@@ -20,7 +19,7 @@ import java.util.logging.Level;
             // Attempt to parse the input as an integer
             arg1 = Integer.parseInt(args[0]);
         } catch (NumberFormatException e) {
-            System.out.println("Error: Non-integer input provided. Please enter a valid integer value.");
+            logger.severe("Error: Non-integer input provided. Please enter a valid integer value.");
             return;
         }
 
@@ -29,7 +28,7 @@ import java.util.logging.Level;
         int rem, num = arg1;
         String hexadecimal = "";
 
-        System.out.println("Converting the Decimal Value " + num + " to Hex...");
+        logger.info("Converting the Decimal Value " + num + " to Hex...");
 
         while (num != 0) {
             rem = num % 16;
@@ -37,7 +36,7 @@ import java.util.logging.Level;
             num = num / 16;
         }
 
-        System.out.println("Hexadecimal representation is: " + hexadecimal);
+        logger.info("Hexadecimal representation is: " + hexadecimal);
         
     }
 }
