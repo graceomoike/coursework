beac0ed622027ee6d16e7893093c35e8e9e78671
graceomoike <graceomoikee@gmail.com>
2024-11-08 16:59:55 +0000

    fixed error

--- TestRunner.java
+++ TestRunner.java
@@ -2,12 +2,9 @@ import org.junit.*;
 import org.junit.runner.JUnitCore;
 import org.junit.runner.Result;
 import org.junit.runner.notification.Failure;
-import java.util.logging.Logger;
-import java.util.logging.Level;
+
 
 public class TestRunner {
-
-    private static final Logger logger = Logger.getLogger(TestRunner.class.getName());
 
     public static void main(String[] args) {
         Result result = JUnitCore.runClasses(Dex2HexTest.class);
@@ -15,27 +12,19 @@ public class TestRunner {
         
         try {
             for (Failure failure : result.getFailures()) {
-                logger.severe(failure.toString());
+                System.out.println(failure.toString());
                 fails++;
             }
             
            if (fails > 0) {
-    		if (logger.isLoggable(Level.SEVERE)) {
-        	logger.severe(String.format("%d tests failed", fails));
+    		throw new Exception(); 
+        	System.out.println(result.wasSuccesful());
     	}
-    	    System.exit(1);
-	} else {
-	   if (logger.isLoggable(Level.INFO)) {
-	        logger.info("All tests passed successfully.");
-    }
-	   System.exit(0);
-}
+	 catch (Exception e) {
+            System.out.println("" + fails + "tests failed");
+ 	    System.exit(1);
+       }
 
-
-
-        } catch (Exception e) {
-            logger.log(Level.SEVERE, "An error occurred during test execution: ", e);
-            System.exit(1);
-        }
+	System.exit(0);
     }
 }
