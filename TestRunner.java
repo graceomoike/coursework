c3ba63d26d6240308575a64382fbbc58ff12ca73
graceomoike <graceomoikee@gmail.com>
2024-11-08 17:26:13 +0000

    fixed error

--- TestRunner.java
+++ TestRunner.java
@@ -2,27 +2,31 @@ import org.junit.*;
 import org.junit.runner.JUnitCore;
 import org.junit.runner.Result;
 import org.junit.runner.notification.Failure;
+import java.lang.exception;
+
 
 public class TestRunner {
 
     public static void main(String[] args) {
         Result result = JUnitCore.runClasses(Dex2HexTest.class);
         int fails = 0;
-        
+	try{        
         // Loop through failures and count them
-        for (Failure failure : result.getFailures()) {
-            System.out.println(failure.toString());
-            fails++;
-        }
-        
-        // Check if any tests failed and print result
-        if (fails > 0) {
-            System.out.println(fails + " tests failed");
+       		 for (Failure failure : result.getFailures()) {
+            		System.out.println(failure.toString());
+            		fails++;
+         	}
+        	if (fails > 0)
+			throw new Exception();
+		System.out.println(result.wasSuccessful());
+	}
+	catch(Exception e)
+        {
+            System.out.println("" + fails + " tests failed");
             System.exit(1);  // Exit with error code if tests failed
-        } else {
-            System.out.println("All tests passed: " + result.wasSuccessful());
+        } 
             System.exit(0);  // Exit with success code if all tests passed
-        }
+        
     }
 }
 
